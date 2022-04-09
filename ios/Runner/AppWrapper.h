7792ba4b394dfa56fa35b9abfc9d47948abb4e9e
umasagashi <hitsuji.santo.issho@gmail.com>
2022-04-09 23:17:51 +0900

    Add native group to ios template

---
+++ ios/Runner/AppWrapper.h
@@ -0,0 +1,12 @@
+#ifndef AppWrapper_h
+#define AppWrapper_h
+
+#import <Foundation/Foundation.h>
+
+@interface AppWrapper : NSObject
+
+-(void)setConfig:(NSString *)config;
+
+@end
+
+#endif /* AppWrapper_h */
