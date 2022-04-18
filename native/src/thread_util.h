b8f12404f416763aae925318a87fdfba68b5ba31
umasagashi <hitsuji.santo.issho@gmail.com>
2022-04-18 23:29:27 +0900

    Add signal-slot utils

---
+++ native/src/thread_util.h
@@ -0,0 +1,47 @@
+#ifndef NATIVE_THREAD_UTIL_H
+#define NATIVE_THREAD_UTIL_H
+
+#include <atomic>
+#include <iostream>
+#include <memory>
+#include <thread>
+
+namespace threading {
+
+class ThreadBase {
+public:
+    ThreadBase()
+        : is_running(false)
+        , thread(nullptr) {}
+
+    virtual ~ThreadBase() { join(); }
+
+    void start() {
+        if (is_running.load()) {
+            return;
+        }
+        is_running.store(true);
+        thread = std::make_unique<std::thread>([this]() { run(); });
+    }
+
+    void join() {
+        if (!is_running.load()) {
+            return;
+        }
+        is_running.store(false);
+        thread->join();
+        thread = nullptr;
+    }
+
+protected:
+    bool isRunning() const { return is_running.load(); }
+    virtual void run() = 0;
+
+private:
+    std::atomic_bool is_running;
+    std::unique_ptr<std::thread> thread;
+};
+
+}  // namespace threading
+
+#endif  //NATIVE_THREAD_UTIL_H
