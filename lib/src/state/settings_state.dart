3a99febd511037f0d9bbf4a85002976df9a57990
umasagashi <hitsuji.santo.issho@gmail.com>
2022-05-09 21:07:33 +0900

    Add sidebar state provider

--- lib/src/state/settings_state.dart
+++ lib/src/state/settings_state.dart
@@ -4,20 +4,28 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../preference/storage_box.dart';
 import 'notifier.dart';
 
-final _storageBoxProvider = Provider<StorageBox>((ref) {
-  return StorageBox(BoxKey.settings);
-});
-
-enum _StorageBoxValueKey {
+enum _SettingsEntryKey {
   themeMode,
+  sidebarExtended,
 }
+
+final _storageBoxProvider = Provider<StorageBox>((ref) {
+  return StorageBox(StorageBoxKey.settings);
+});
 
 final themeSettingProvider = StateNotifierProvider<ExclusiveItemsNotifier<ThemeMode>, ThemeMode>((ref) {
   final box = ref.watch(_storageBoxProvider);
   return ExclusiveItemsNotifier<ThemeMode>(
-    boxValueKey: _StorageBoxValueKey.themeMode.name,
-    box: box,
+    entry: StorageEntry(box: box, key: _SettingsEntryKey.themeMode.name),
     values: [ThemeMode.light, ThemeMode.dark, ThemeMode.system],
     defaultValue: ThemeMode.system,
+  );
+});
+
+final sidebarExtendedStateProvider = StateNotifierProvider<BooleanNotifier, bool>((ref) {
+  final box = ref.watch(_storageBoxProvider);
+  return BooleanNotifier(
+    entry: StorageEntry(box: box, key: _SettingsEntryKey.sidebarExtended.name),
+    defaultValue: true,
   );
 });
