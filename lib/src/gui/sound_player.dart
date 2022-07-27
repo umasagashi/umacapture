b85d2eaa42b5fd0dd8b94102b650eece5e7a75ce
umasagashi <hitsuji.santo.issho@gmail.com>
2022-07-28 03:45:41 +0900

    Fix android notification sound

--- lib/src/gui/sound_player.dart
+++ lib/src/gui/sound_player.dart
@@ -73,7 +73,8 @@ class SoundEffect {
   static load(SoundSetting setting) async {
     final player = AudioPlayer();
     await player.setReleaseMode(ReleaseMode.stop);
-    await player.setPlayerMode(PlayerMode.lowLatency);
+    // TODO: This does not work on android.
+    // await player.setPlayerMode(PlayerMode.lowLatency);
     await player.setSourceAsset(setting.path);
     await player.setVolume(setting.volume);
     return SoundEffect._(player);
