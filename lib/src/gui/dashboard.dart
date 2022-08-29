e5d3dfcc5ef7fbbe189999ba4749ea9d8d1a9401
umasagashi <umasagashi.dev@gmail.com>
2022-08-30 00:40:11 +0900

    Fix updater loader

--- lib/src/gui/dashboard.dart
+++ lib/src/gui/dashboard.dart
@@ -1,12 +1,12 @@
 import 'package:dio/dio.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:markdown/markdown.dart' as md;
 import 'package:percent_indicator/percent_indicator.dart';
-import 'package:version/version.dart';
 
 import '/const.dart';
 import '/src/core/path_entity.dart';
@@ -34,64 +34,66 @@ final _newsMarkdownLoader = FutureProvider<String>((ref) async {
 });
 
 class AppUpdaterGroup extends ConsumerWidget {
-  final Version version;
+  final String version;
 
   const AppUpdaterGroup({Key? key, required this.version}) : super(key: key);
 
   void downloadAndOpen(WidgetRef ref) {
     ref.read(_downloadProgressProvider.notifier).update((_) => Progress(count: 0, total: 100));
-    final isInstallerMode = ref.watch(isInstallerModeLoader).asData!.value;
-    final pathInfo = ref.watch(pathInfoProvider);
-    final downloadUrl =
-        isInstallerMode ? Const.appExeUrl(version: version.toString()) : Const.appZipUrl(version: version.toString());
-    final FilePath downloadPath = pathInfo.downloadDir.filePath(Uri.parse(downloadUrl).pathSegments.last);
-    logger.d("$downloadUrl, ${downloadPath.path}");
-    Dio().download(
-      downloadUrl,
-      downloadPath.path,
-      onReceiveProgress: (int count, int total) {
-        ref.read(_downloadProgressProvider.notifier).update((_) => Progress(count: count, total: total));
-      },
-    ).then((_) {
-      ref.read(_downloadProgressProvider.notifier).update((_) => null);
-      (isInstallerMode ? downloadPath : downloadPath.parent).launch();
+    ref.watch(isInstallerModeLoader.future).then((isInstallerMode) {
+      final pathInfo = ref.watch(pathInfoProvider);
+      final downloadUrl = isInstallerMode ? Const.appExeUrl(version: version) : Const.appZipUrl(version: version);
+      final FilePath downloadPath = pathInfo.downloadDir.filePath(Uri.parse(downloadUrl).pathSegments.last);
+      logger.d("$downloadUrl, ${downloadPath.path}");
+      Dio().download(
+        downloadUrl,
+        downloadPath.path,
+        onReceiveProgress: (int count, int total) {
+          ref.read(_downloadProgressProvider.notifier).update((_) => Progress(count: count, total: total));
+        },
+      ).then((_) {
+        ref.read(_downloadProgressProvider.notifier).update((_) => null);
+        (isInstallerMode ? downloadPath : downloadPath.parent).launch();
+      });
     });
   }
 
   Widget downloadProgressWidget(BuildContext context, WidgetRef ref, Progress progress) {
     final theme = Theme.of(context);
-    final isInstallerMode = ref.watch(isInstallerModeLoader).asData!.value;
-    return Flex(
-      direction: Axis.horizontal,
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8),
-          child: CircularPercentIndicator(
-            radius: 32.0,
-            lineWidth: 6.0,
-            animation: true,
-            animateFromLastPercent: true,
-            animationDuration: 200,
-            percent: progress.progress,
-            center: Text("${progress.percent}%"),
-            progressColor: theme.colorScheme.primary,
-            backgroundColor: theme.colorScheme.secondaryContainer,
+    return ref.watch(isInstallerModeLoader).guarded((isInstallerMode) {
+      return Flex(
+        direction: Axis.horizontal,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: CircularPercentIndicator(
+              radius: 32.0,
+              lineWidth: 6.0,
+              animation: true,
+              animateFromLastPercent: true,
+              animationDuration: 200,
+              percent: progress.progress,
+              center: Text("${progress.percent}%"),
+              progressColor: theme.colorScheme.primary,
+              backgroundColor: theme.colorScheme.secondaryContainer,
+            ),
           ),
-        ),
-        Flexible(
-          child: Text("$tr_dashboard.app_updater.downloading.template".tr(namedArgs: {
-            "file": isInstallerMode
-                ? "$tr_dashboard.app_updater.downloading.exe".tr()
-                : "$tr_dashboard.app_updater.downloading.zip".tr(),
-          })),
-        ),
-      ],
-    );
+          Flexible(
+            child: Text("$tr_dashboard.app_updater.downloading.template".tr(namedArgs: {
+              "file": isInstallerMode
+                  ? "$tr_dashboard.app_updater.downloading.exe".tr()
+                  : "$tr_dashboard.app_updater.downloading.zip".tr(),
+            })),
+          ),
+        ],
+      );
+    });
   }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final theme = Theme.of(context);
     final downloadProgress = ref.watch(_downloadProgressProvider);
     return ListCard(
       title: "$tr_dashboard.app_updater.title".tr(),
@@ -99,6 +101,7 @@ class AppUpdaterGroup extends ConsumerWidget {
       children: [
         ListTile(
           title: Text("$tr_dashboard.app_updater.subtitle".tr()),
+          tileColor: theme.colorScheme.surface.blend(Colors.yellow, 15),
           onTap: downloadProgress != null ? null : () => downloadAndOpen(ref),
         ),
         AnimatedSwitcher(
@@ -178,7 +181,7 @@ class DashboardPage extends ConsumerWidget {
     final AppVersionCheckResult? result = ref.watch(appVersionCheckLoader).asData?.value;
     return ListTilePageRootWidget(
       children: [
-        if (result?.isUpdatable ?? false) AppUpdaterGroup(version: result!.latest),
+        if (result?.isUpdatable ?? false) AppUpdaterGroup(version: result!.latest.toString()),
         const _NewsGroup(),
         const _StatisticGroup(),
       ],
