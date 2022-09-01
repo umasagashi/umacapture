fb5e378ddc6a759dca62ab1781df65ed06aaa90b
umasagashi <umasagashi.dev@gmail.com>
2022-09-01 23:15:44 +0900

    Refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:dart_json_mapper/dart_json_mapper.dart';
 import 'package:easy_localization/easy_localization.dart';
+
+// ignore: depend_on_referenced_packages
+import 'package:easy_logger/easy_logger.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logger/logger.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:window_manager/window_manager.dart';
 
@@ -15,6 +19,56 @@ import 'src/core/version_check.dart';
 import 'src/gui/app_widget.dart';
 import 'src/preference/storage_box.dart';
 import 'src/preference/window_state.dart';
+
+extension LevelMessagesExtension on LevelMessages {
+  Level get asLoggerLevel {
+    switch (this) {
+      case LevelMessages.debug:
+        return Level.debug;
+      case LevelMessages.info:
+        return Level.info;
+      case LevelMessages.warning:
+        return Level.warning;
+      case LevelMessages.error:
+        return Level.error;
+    }
+  }
+}
+
+void easyLocalizationPrinter(Object object, {String? name, StackTrace? stackTrace, LevelMessages? level}) {
+  final loggerLevel = level?.asLoggerLevel ?? EasyLocalization.logger.defaultLevel.asLoggerLevel;
+  final message = "$name: ${object.toString()}";
+  if (stackTrace == null) {
+    logger.log(loggerLevel, message);
+  } else {
+    logger.log(loggerLevel, message, "EasyLocalizationError", stackTrace);
+  }
+}
+
+void setupLocalization() async {
+  EasyLocalization.logger = EasyLogger(
+    name: "Easy Localization",
+    printer: easyLocalizationPrinter,
+    enableLevels: [
+      // LevelMessages.debug,
+      LevelMessages.info,
+      LevelMessages.error,
+      LevelMessages.warning,
+    ],
+  );
+  await EasyLocalization.ensureInitialized();
+}
+
+void setupLicense() {
+  rootBundle.loadString("assets/additional_license_info.json").then((infoString) {
+    final info = JsonMapper.deserialize<Map<String, dynamic>>(infoString)!.map((k, v) => MapEntry(k, v.toString()));
+    LicenseRegistry.addLicense(() async* {
+      for (final entry in info.entries) {
+        yield LicenseEntryWithLineBreaks([entry.key], await rootBundle.loadString(entry.value));
+      }
+    });
+  });
+}
 
 void setupWindowManager() async {
   await windowManager.ensureInitialized();
@@ -48,17 +102,6 @@ void setupWindowManager() async {
   );
 }
 
-void setupLicense() {
-  rootBundle.loadString("assets/additional_license_info.json").then((infoString) {
-    final info = JsonMapper.deserialize<Map<String, dynamic>>(infoString)!.map((k, v) => MapEntry(k, v.toString()));
-    LicenseRegistry.addLicense(() async* {
-      for (final entry in info.entries) {
-        yield LicenseEntryWithLineBreaks([entry.key], await rootBundle.loadString(entry.value));
-      }
-    });
-  });
-}
-
 void run() {
   runApp(
     ProviderScope(
@@ -68,6 +111,8 @@ void run() {
       // EasyLocalization must be placed at the root. Otherwise, hot reload will not work for some reason.
       child: EasyLocalization(
         path: "assets/translations",
+        useOnlyLangCode: true,
+        useFallbackTranslations: true,
         supportedLocales: const [
           Locale('ja'),
         ],
@@ -82,10 +127,8 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   initializeJsonReflectable();
   await StorageBox.ensureOpened(reset: false);
-  await EasyLocalization.ensureInitialized();
-
+  setupLocalization();
   setupLicense();
-
   if (CurrentPlatform.hasWindowFrame()) {
     setupWindowManager();
   }
